c1a370d8c40a9877ab96fb20384a7c4e8e34ea1b
applqpak <applqpak@gmail.com>
2016-05-24 15:51:14 +1000

    A LOT of improvements.

--- src/GlacierShard92/Mining/Main.php
+++ src/GlacierShard92/Mining/Main.php
@@ -8,7 +8,7 @@ use pocketmine\plugin\Plugin;
 
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\command\{Command, CommandSender};
+use pocketmine\command\{ConsoleCommandSender, Command, CommandSender};
 
 use pocketmine\item\Item;
 use pocketmine\utils\TextFormat as C;
@@ -24,30 +24,27 @@ class Main extends PluginBase implements Listener{
   }
   public function OnBreak(BlockBreakEvent $event){
    $name = $event->getPlayer()->getName();
-   $breakdata = new Config($this->getDataFolder() . "/breaks.yml", Config::YAML);
    $breaks = $breakdata->get($name);
-       $breakdata->set($name,$breaks+1);
-       $breakdata->save();
+       $breakfile->set($name,$breaks+1);
+       $breakfile->save();
        if($this->$name,$breaks == 64){
           $event->getPlayer()->sendMessage(C::YELLOW . "ERPE MCMMO - Mining skill increased by 1 by mining 64 blocks!");
           $event->getPlayer()->sendMessage(C::YELLOW . "ERPE MCMMO - You now have 32 Bottle o' Enchanting in your inventory!");
-          $this->getServer()->dispatchCommand("give $name 384 32");
+          $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "give $name 384 32");
           
        }else{
-          $this->getServer()->dispatchCommand("give $name 384 32");
+          $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "give " . $name . "384 32");
        }
   public funtion onCommand(CommandSender $s, Command $cmd, $label, array $args){
-        if($cmd->getName() == "mining"){
+        if(strtolower($cmd->getName()) == "mining"){
           if($s instanceof Player){
           if(count($args) == 0) {
-            $breakfile = new Config($this->getDataFolder() . "/breaks.yml", Config::YAML);
             $breaks = $breakfile->get($s->getName());
             $miningstats = C::YELLOW . "§lYour Mining McMMO:§r§b $breaks";
             $s->sendMessage($miningstats);
           }
           if(count($args) == 1){
             $player = $args[0];
-            $breakfile = new Config($this->getDataFolder() . "/breaks.yml", Config::YAML);
             $breaks = $breakfile->get($player);
             $miningstats = C::YELLOW . C::BOLD . $args[0] . "'s' Mining McMMO:§r§b $breaks";
           }
