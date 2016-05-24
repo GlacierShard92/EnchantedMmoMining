e1a7a5d2cdc835e3e80c683533bc416efc1997f1
Joe <JoeycraftPE@gmail.com>
2016-05-24 18:05:19 -0500

    SPELLING MISTAKE
    SPELLING MISTAKE

--- src/GlacierShard92/Mining/Main.php
+++ src/GlacierShard92/Mining/Main.php
@@ -35,7 +35,7 @@ class Main extends PluginBase implements Listener {
        }else{
           $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "give " . $name . "384 10");
        }
-  public funtion onCommand(CommandSender $s, Command $cmd, $label, array $args){
+  public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         if(strtolower($cmd->getName()) == "mining"){
           if($s instanceof Player){
           if(count($args) == 0) {
