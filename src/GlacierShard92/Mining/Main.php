905ab0e6914529398925176f66f977d5781d2fea
Joe [ERPE] <JoeycraftPE@gmail.com>
2016-07-03 18:27:15 -0500

    Beta test for levels

--- src/GlacierShard92/Mining/Main.php
+++ src/GlacierShard92/Mining/Main.php
@@ -8,7 +8,7 @@ use pocketmine\plugin\Plugin;
 
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\command\{ConsoleCommandSender, Command, CommandSender};
+use pocketmine\command\{Command, CommandSender};
 use pocketmine\event\block\BlockBreakEvent;
 
 use pocketmine\item\Item;
@@ -25,18 +25,31 @@ class Main extends PluginBase implements Listener {
   }
   public function OnBreak(BlockBreakEvent $event){
    $name = $event->getPlayer()->getName();
+   $inventory = $player->getInventory();
    $breaks = $this->breakfile->get($name);
        $this->breakfile->set($name,$breaks+1);
        $this->breakfile->save();
        if($breaks == 20){
           $event->getPlayer()->sendMessage("§l§e[§r§2ERPE Mining McMMO§e§l]");
-          $event->getPlayer()->sendMessage("§aMining level now level 1! You've recieved 10 xp bottles in return!");
-          $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "give " . $name . "384 10");
-          
-       }else{
-          $this->getServer()->dispatchCommand(new ConsoleCommandSender(), "give " . $name . "384 10");
-       }
-  }
+          $event->getPlayer()->sendMessage("§aMining level now level 1! You've recieved 10 xp bottles!");
+          $inventory->addItem(Item::(384,0,10));
+        if($breaks = 45){
+          $event->getPlayer()->sendMessage("§l§e[§r§2ERPE Mining McMMO§e§l]");
+          $event->getPlayer()->sendMessage("§aMining level now level 2! You've recieved 5 iron ingots!");
+          $inventory->addItem(Item::(265,0,5));
+        if($breaks = 60){
+          $event->getPlayer()->sendMessage("§l§e[§r§2ERPE Mining McMMO§e§l]");
+          $event->getPlayer()->sendMessage("§aMining Level now level 3! You've recieved 3 Basic Crate Keys!");
+          $inventory->addItem(Item::(341,0,3));
+        if($breaks = 100){
+          $event->getPlayer()->sendMessage("§l§e[§r§2ERPE Mining McMMO§e§l]");
+          $event->getPlayer()->sendMessage("§aMining Level now level 4! You've recieved 10 Diamonds!");
+          $event->getPlayer()->sendMessage("§bCONGRATS! You reached the max level for the Beta! More mining levels to come in the next update!");
+          $inventory->addItem(Item::(364,0,10));
+        }
+      }
+    }
+ }
   public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         if(strtolower($cmd->getName()) == "mining"){
           if($s instanceof Player){
@@ -49,8 +62,8 @@ class Main extends PluginBase implements Listener {
           if(count($args) == 1){
             $player = $args[0];
             $breaks = $this->breakfile->get($player);
-            $s->sendMessage("§e§l[§r§eERPE Mining McMMO§e§l]");
-            $s->sendMessage("e§l " . $args[0] . "'s Broken Blocks:§r§b " . $breaks);
+            $s->sendMessage("§e§l[§r§2ERPE Mining McMMO§e§l]");
+            $s->sendMessage("§e§l " . $args[0] . "'s Broken Blocks:§r§b " . $breaks);
             return;
           }
         }
