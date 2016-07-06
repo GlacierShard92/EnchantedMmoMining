ffbf807d52e29484fb250fe4ac791da9900921b0
Joe [ERPE] <JoeycraftPE@gmail.com>
2016-07-06 12:30:55 -0500

    unexpected public fix (?)

--- src/GlacierShard92/Mining/Main.php
+++ src/GlacierShard92/Mining/Main.php
@@ -84,6 +84,7 @@ class Main extends PluginBase implements Listener {
      }
      }
     }
+    }
       public function onCommand(CommandSender $s, Command $cmd, $label, array $args){
         switch(strtolower($cmd->getName())) {
           case "mining":
